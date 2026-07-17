1d291386d0cab848691334608ef771b8d26522dd
Abdullah Khan <abdullahali1342@gmail.com>
2026-07-18 02:29:13 +0500

    update addshop handler

--- frontend/src/axios/addEditShop.js
+++ frontend/src/axios/addEditShop.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setMyShopData } from "../redux/ownerSlice.js";
 
 const serverUrl = "http://localhost:3000"
 
@@ -7,9 +8,12 @@ export const handleAddEditShop = async(formData, dispatch) => {
     try{
         const result = await axios.post(`${serverUrl}/api/shop/create-edit-shop`,
             formData,
-            {headers: {'Content-Type': 'multipart/form-data'}, withCredentials: true}
+            {
+                headers: { 'Content-Type': 'multipart/form-data' },
+                withCredentials: true
+            }
         )
-        dispatch(setMyShopData(result.data.shop))
+        dispatch(setMyShopData(result.data.shop || result.data.updatedShop))
     } catch(err) {
         throw err;
     }
