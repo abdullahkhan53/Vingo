375825b57c2418f673e61d3adeff43c257fe9ca9
Abdullah Khan <abdullahali1342@gmail.com>
2026-07-28 03:05:48 +0500

    category completed

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Category from "./category/Category";
 import { categories } from "../../utils/category.js";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
@@ -7,11 +7,63 @@ import Navbar from "./Navbar";
 
 function UserDashboard() {
 
-    const cateScrolllRef = useRef();
+    const cateScrollRef = useRef();
+    const [isRightScroll, setIsRightScroll] = useState(false);
+    const [isLeftScroll, setIsLeftScroll] = useState(false);
 
     const  handleCateScroll = (ref, direction) => {
-        console.log(ref.current, direction);
+        try {
+            if (ref.current) {
+            ref.current.scrollBy({
+                left: direction == "left" ? - 250 : 250,
+                behavior: "smooth"
+            })            
+        }
+        } catch(err) {
+            console.log(err);
+        }
     }
+
+    const handleScrollBtn = () => {
+        try{
+            if(!cateScrollRef.current) return;
+
+            let {scrollLeft, clientWidth, scrollWidth} = cateScrollRef.current;
+            let result = scrollLeft + clientWidth >= scrollWidth - 1;
+
+            if(result) {
+                setIsRightScroll(true);
+            }  else {
+                setIsRightScroll(false)
+            };
+
+            if(scrollLeft <= 5) {
+                setIsLeftScroll(true);
+            } else {
+                setIsLeftScroll(false)
+            };
+
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect( () => {
+        try{
+            if(!cateScrollRef.current) return;
+            const cateScrollContainer = cateScrollRef.current;
+           
+           handleScrollBtn();
+
+            cateScrollContainer.addEventListener("scroll", handleScrollBtn);
+           
+           return () => {
+            cateScrollContainer.removeEventListener("scroll", handleScrollBtn);
+           }
+        } catch(err) {
+            console.log(err);
+        }
+    }, [])
 
     return(
         <div className="w-screen min-h-screen flex flex-col items-center bg-[#fff9f6] overflow-y-auto">
@@ -22,22 +74,30 @@ function UserDashboard() {
 
                 {/* Section content goes here */}
                 <div className="w-full relative">
-                    <button className="absolute left-0 top-[50%] translate-y-[-50%] z-10 text-white bg-[#ff4d2d]
-                     rounded-full p-1" onClick={handleCateScroll(cateScrolllRef, "left")}>
-                        
-                        <FaChevronCircleLeft size={30}  />
-                    </button>
+
+                    {
+                        !isLeftScroll && 
+                        <button className="absolute left-0 top-[50%] translate-y-[-50%] z-10 text-white bg-[#ff4d2d]
+                        rounded-full p-1 cursor-pointer" onClick={() => handleCateScroll(cateScrollRef, "left")}>
+                            
+                            <FaChevronCircleLeft size={30}  />
+                        </button>
+                    }
                     {/* Category Component */}
-                    <div className="w-full flex items-center gap-4 overflow-x-auto  pb-2" ref={cateScrolllRef}>
+                    <div className="w-full flex items-center gap-4 overflow-x-auto  pb-2" ref={cateScrollRef}>
                         {
                             categories.map((category, index) => {
                                 return <Category data={category} key={index}/>;
                             })
                         }
                     </div>
-                    <button className="absolute right-0 top-[50%] translate-y-[-50%] z-10 text-white bg-[#ff4d2d] rounded-full p-1">
-                        <FaChevronCircleRight size={30} />
-                    </button>
+                    {
+                        !isRightScroll && 
+                        <button className="absolute right-0 top-[50%] translate-y-[-50%] z-10 text-white bg-[#ff4d2d]
+                        rounded-full p-1 cursor-pointer"  onClick={() => handleCateScroll(cateScrollRef, "right")}>
+                            <FaChevronCircleRight size={30} />
+                        </button>
+                    }
                 </div>
 
             </div>
--- frontend/src/components/category/Category.jsx
+++ frontend/src/components/category/Category.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
 function Category({ data }) {
+
+    const  handleCateScroll = (ref, direction) => {
+        try {
+            if (ref.current) {
+            ref.current.scrollBy({
+                left: direction == "left" ?  -200 : 200,
+                behavior: "smooth"
+            })
+        }
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
         <div className="w-[120px] h-[120px] md:w-[180px] md:h-[180px] bg-white shadow-xl
          border-2 border-[#ff4d2d] shadow-gray rounded-2xl  shrink-0 overflow-hidden
-          gap-[10px] hover:shadow-lg transition-shadow">
+          gap-[10px] hover:shadow-lg transition-shadow relative hover:translate-y-[-8px] duration-1000">
             <div className="w-full h-full flex flex-col gap-[10px] cursor-pointer">
-                <div className="w-[100%] h-[90%] md:h-[80%] overflow-hidden">
+                <div className="w-[100%] h-[100%]  overflow-hidden hover:scale-[1.2] transition-transform duration-300">
                 <img src={data.image} alt={data.category} className="w-[100%] h-full object-cover"/>
             </div>
-            <div className="w-[100%] h-[10%] flex items-center justify-center">
+            <div className="w-[100%] h-[15%] flex items-center justify-center absolute bottom-0 left-0
+             bg-white/60 backdrop-blur-md">
                 <h1 className="text-gray-800 text-sm md:text-lg font-semibold">{data.category}</h1>
             </div>
             </div>
