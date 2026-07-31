39a42b8a91f132c8af5fc413b8b6bb26daf7eb5a
Abdullah Khan <abdullahali1342@gmail.com>
2026-08-01 03:03:31 +0500

    test

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -7,6 +7,6 @@ const itemRouter = express.Router();
 itemRouter.post("/add-item",isAuth, upload.single("image"), createItem);
 itemRouter.post("/edit-item/:itemId",isAuth, upload.single("image"), editItem);
 itemRouter.post("/delete-item/:itemId",isAuth,  deleteItem);
-itemRouter.get("/get-item-by-id/:itemId",isAuth,  getItemById);
+itemRouter.get("/get-item-by-id/:itemId",isAuth,  getItemById)
 
 export default itemRouter;
